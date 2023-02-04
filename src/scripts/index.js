3488c43be18f47b2a54ef4907b20bc1ca7100e97
Dean <deaneffi1414@gmail.com>
2023-02-04 20:34:48 +0200

    commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,113 +1,38 @@
-function Ship(name,length){
-    let hits = 0
-    let sank = false 
-    function hit(){
-        hits++
-        if(hits >= length){
-            this.sink()
-        }
-    }
-    function sink(){
-        this.sank = true 
-        console.log(this.name + ' HAS SANKKKK!');
-    }
-    return{name,length,hit,sank,sink}
-
-}
-
-function GameBoard(){
-    const attacksMissed = []
-    const attacksHit =  []
-    const map = []
-    const row = [0,0,0,0,0,0,0,0,0,0]
-    for (let i = 0; i < 10; i++) {
-        map.push([...row])
-    }
-
-    function addShip(ship,coor,direction = "horizontal"){
-        if(direction == "horizontal"){  
-            //check if coors available
-            for(let j = 0; j < ship.length; j++){
-                if(coor[0] + j > 9 || coor[0] + j < 0 || coor[1] + j > 9 || coor[1] + j < 0 
-                || this.map[coor[0]][coor[1] + j]){
-
-                    return false
-                }
-            }
-            
-            //add Ship To Board
-            for(let i = 0; i < ship.length; i++){
-                this.map[coor[0]][coor[1] + i] = ship.name
-            }
-        }else{
-
-            //check if coors available
-            for(let j = 0; j < ship.length; j++){
-                if(coor[0] + j > 9 || coor[0] + j < 0 || coor[1] + j > 9 || coor[1] + j < 0
-                || this.map[coor[0] + j][coor[1]]){
-
-                    return "Spot is taken"
-                }
-            }
-            
-            //add Ship To Board
-            for(let i = 0; i < ship.length; i++){
-                this.map[coor[0] + i][coor[1]] = ship.name
-            }
-        }
-    }
+import{Player,Ship,GameBoard} from "./main.js"
 
 
-    function reciveAttack(coors){
+// console.log({Player,Ship,GameBoard});
+let turn;
 
-        //checks if already shot
-        for(let arr of [...attacksHit, ...attacksMissed]){
-            if(arr[0] == coors[0]){
-                if (arr[1] == coors[1]){
-                    console.log('already shot there ');
-                    return true
-                }
-            }
-        }
+//temporary way to initiate game
+let playerGameBoard = GameBoard()
+let botGameBoard = GameBoard()
 
-
-
-        //evaluates if the shot hit or missed 
-        if(this.map[coors[0]][coors[1]]){
-            attacksHit.push(coors)
-            //turn name string into variable to hit correct ship 
-            eval(this.map[coors[0]][coors[1]]).hit()
-        
-
-            return true
-        }else{
-            attacksMissed.push(coors)
-            return false
-        }
-    }
-
-
-    return{map,addShip,reciveAttack}
-}
-
-let destroyer  = Ship("destroyer",3)
-let bigBoy  = Ship("bigBoy",5)
-
-let gameBoard1 = GameBoard()
-
-gameBoard1.addShip(destroyer,[0,2],"vertical")
-gameBoard1.addShip(bigBoy,[0,5],"horizontal")
-
-gameBoard1.reciveAttack([0,2])
-gameBoard1.reciveAttack([1,2])
-
-gameBoard1.reciveAttack([2,2])
-
-
-gameBoard1.reciveAttack([3,2])
+let patrolBoat =Ship("patrolBoat",2)
+let submarine = Ship("submarine",3)
+let destroyer = Ship("destroyer",3)
+let battleShip = Ship("battleShip",4)
+let carrier = Ship("carrier",5)
 
 
 
 
 
-export{Ship,GameBoard}
+playerGameBoard.addShip(patrolBoat,[4,0])
+playerGameBoard.addShip(submarine,[2,5],"vertical")
+playerGameBoard.addShip(destroyer,[1,0])
+playerGameBoard.addShip(battleShip,[6,0])
+playerGameBoard.addShip(carrier,[5,4])
+
+
+botGameBoard.addShip(patrolBoat,[4,0])
+botGameBoard.addShip(submarine,[2,5],"vertical")
+botGameBoard.addShip(destroyer,[1,0])
+botGameBoard.addShip(battleShip,[6,0])
+botGameBoard.addShip(carrier,[5,4])
+
+
+
+
+
+console.log({botGameBoard,playerGameBoard});
---
+++ src/scripts/main.js
@@ -0,0 +1,91 @@
+function Ship(name,length){
+    let hits = 0
+    let sank = false 
+    function hit(){
+        hits++
+        if(hits >= length){
+            this.sink()
+        }
+    }
+    function sink(){
+        this.sank = true 
+        console.log(this.name + ' HAS SANKKKK!');
+    }
+    return{name,length,hit,sank,sink}
+
+}
+function GameBoard(){
+    const attacksMissed = []
+    const attacksHit =  []
+    const map = []
+    const row = [0,0,0,0,0,0,0,0,0,0]
+    for (let i = 0; i < 10; i++) {
+        map.push([...row])
+    }
+
+    function addShip(ship,coor,direction = "horizontal"){
+        if(direction == "horizontal"){  
+            //check if coors available
+            for(let j = 0; j < ship.length; j++){
+                if(coor[0] + j > 9 || coor[0] + j < 0 || coor[1] + j > 9 || coor[1] + j < 0 
+                || this.map[coor[0]][coor[1] + j]){
+
+                    return false
+                }
+            }
+            //add Ship To Board
+            for(let i = 0; i < ship.length; i++){
+                this.map[coor[0]][coor[1] + i] = ship.name
+            }
+        }else{
+
+            //check if coors available
+            for(let j = 0; j < ship.length; j++){
+                if(coor[0] + j > 9 || coor[0] + j < 0 || coor[1] + j > 9 || coor[1] + j < 0
+                || this.map[coor[0] + j][coor[1]]){
+
+                    return "Spot is taken"
+                }
+            }
+            
+            //add Ship To Board
+            for(let i = 0; i < ship.length; i++){
+                this.map[coor[0] + i][coor[1]] = ship.name
+            }
+        }
+    }
+    function reciveAttack(coors){
+
+        //checks if already shot
+        for(let arr of [...attacksHit, ...attacksMissed]){
+            if(arr[0] == coors[0]){
+                if (arr[1] == coors[1]){
+                    console.log('already shot there ');
+                    return true
+                }
+            }
+        }
+        //evaluates if the shot hit or missed 
+        if(this.map[coors[0]][coors[1]]){
+            attacksHit.push(coors)
+            //turn name string into variable to hit correct ship 
+            eval(this.map[coors[0]][coors[1]]).hit()
+        
+
+            return true
+        }else{
+            attacksMissed.push(coors)
+            return false
+        }
+    }
+    return{map,addShip,reciveAttack}
+}
+
+function Player(name="player1"){
+    let turn = false
+    let ships = []
+
+    return{name,turn,shipsPlaced}
+}
+
+export{Ship,GameBoard,Player}
